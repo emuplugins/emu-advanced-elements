4b997056cde7fdb2c2f4ec2e486deec2776ff1aa
Emu Plugins <147978343+emuplugins@users.noreply.github.com>
2025-03-31 15:45:01 -0300

    Update elements-loader.php

--- builders/Bricks/elements-loader.php
+++ builders/Bricks/elements-loader.php
@@ -2,27 +2,25 @@
 
 if ( ! defined('ABSPATH')) exit;
 
-// Verifica se o Bricks Builder está ativo
-if ( class_exists( 'Bricks\Elements' ) ) {
+if ( get_template() !== 'bricks' ) return;
 
-    // Registra o elemento personalizado
-    add_action( 'init', function() {
-        $elements = [
-            [
-                'slug' => 'emu-marquee',
-                'class' => 'Emu_Marquee_Widget',
-                'file' => 'widgets/advanced-marquee.php'
-            ],
-            // [
-            //     'slug' => 'emu-gallery-lightbox',
-            //     'class' => 'Emu_Gallery_Lightbox',
-            //     'file' => 'widgets/gallery-lightbox.php'
-            // ]
-        ];
+// Registra o elemento personalizado
+add_action( 'init', function() {
+    $elements = [
+        [
+            'slug' => 'emu-marquee',
+            'class' => 'Emu_Marquee_Widget',
+            'file' => 'widgets/advanced-marquee.php'
+        ],
+        // [
+        //     'slug' => 'emu-gallery-lightbox',
+        //     'class' => 'Emu_Gallery_Lightbox',
+        //     'file' => 'widgets/gallery-lightbox.php'
+        // ]
+    ];
 
-        foreach ( $elements as $element ) {
-            // Registra o elemento passando o caminho completo do arquivo
-            \Bricks\Elements::register_element( (plugin_dir_path( __FILE__ ).$element['file'] ), $element['slug'], $element['class'] );
-        }
-    }, 11 );
-}
+    foreach ( $elements as $element ) {
+        // Registra o elemento passando o caminho completo do arquivo
+        \Bricks\Elements::register_element( (plugin_dir_path( __FILE__ ).$element['file'] ), $element['slug'], $element['class'] );
+    }
+}, 11 );
